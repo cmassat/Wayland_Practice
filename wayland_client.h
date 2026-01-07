a3280ea1ed43bc770719476e489468d922205ff6
cmassat <bill.massat@outlook.com>
2026-01-06 21:47:00 -0500

    Avoid blocking Wayland dispatch in render loop

--- wayland_client.h
+++ wayland_client.h
@@ -8,5 +8,5 @@ struct window {
 struct window wayland_create_window();
 bool wayland_dispatch_complete();
 void wayland_render_frame();
-void *waylen_create_buffer();
+void *waylen_create_buffer(int width, int height);
 void wayland_destroy_buffer();
